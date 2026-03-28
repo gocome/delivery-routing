5da55f0739a51343b9d33597351fcee74a955e53
Xin Chen <xin.chen.ntu@gmail.com>
2026-03-28 12:51:09 -0400

    output format for delivery instead of pickup

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def create_data_model(data_file='./data/cvrp_problem_data.json'):
 
 def print_solution(data, manager, routing, solution):
     """Prints solution on console."""
-    print(f"Objective: {solution.ObjectiveValue()}")
+    print(f"Objective: {solution.ObjectiveValue()}\n")
     total_distance = 0
     total_load = 0
     for vehicle_id in range(data["num_vehicles"]):
@@ -31,22 +31,29 @@ def print_solution(data, manager, routing, solution):
         plan_output = f"Route for vehicle {vehicle_id}:\n"
         route_distance = 0
         route_load = 0
+        node_index_list, route_load_list = [], []
         while not routing.IsEnd(index):
             node_index = manager.IndexToNode(index)
             route_load += data["demands"][node_index]
-            plan_output += f" {node_index} Load({route_load}) -> "
+            node_index_list.append(node_index)
+            route_load_list.append(route_load)
+            # plan_output += f" {node_index} Load({route_load}) -> "
             previous_index = index
             index = solution.Value(routing.NextVar(index))
-            route_distance += routing.GetArcCostForVehicle(
-                previous_index, index, vehicle_id
-            )
-        plan_output += f" {manager.IndexToNode(index)} Load({route_load})\n"
-        plan_output += f"Distance of the route: {route_distance}m\n"
+            route_distance += routing.GetArcCostForVehicle(previous_index, index, vehicle_id)
+        node_index_list.append(manager.IndexToNode(index))
+        route_max_load = route_load_list[-1]
+        route_load_list = [route_max_load - load for load in route_load_list]
+        for i, load in enumerate(route_load_list):
+            node_index = node_index_list[i]
+            plan_output += f" {node_index} Load({load}) -> "
+        plan_output += f" {node_index_list[-1]}\n"
+        plan_output += f"Distance of the route: {route_distance}\n"
         plan_output += f"Load of the route: {route_load}\n"
         print(plan_output)
         total_distance += route_distance
         total_load += route_load
-    print(f"Total distance of all routes: {total_distance}m")
+    print(f"Total distance of all routes: {total_distance}")
     print(f"Total load of all routes: {total_load}")
 
 
